2ebc7c524c49a2b6fbe5384bd3ddc04f80e8648f
Omkar Deshpande <omkard199@gmail.com>
2019-09-23 17:39:14 +0530

    Display broadcasted messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,8 +5,7 @@
 	def create
 		message = current_user.messages.build(message_params)
 		if message.save
-			# redirect_to root_path
-			ActionCable.server.broadcast "chatroom_channel", foo: message.body
+			ActionCable.server.broadcast "chatroom_channel", mod_message: message_render(message)
 		end
 	end
 
@@ -14,6 +13,10 @@
 
 	def message_params
 		params.require(:message).permit(:body) 
+	end
+
+	def message_render(message)
+		render(partial: 'message', locals: {message: message})
 	end
 end
 
