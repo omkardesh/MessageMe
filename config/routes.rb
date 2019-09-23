4de04ed9e22e91a5641b429f5045e375c4131546
Omkar Deshpande <omkard199@gmail.com>
2019-09-23 18:37:29 +0530

    Add messages from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   root 'chatroom#index'
-  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new'   #controller#action
   post 'login', to: 'sessions#create'
 	delete'logout', to: 'sessions#destroy'
+	post 'message', to: 'messages#create'
 
 end
